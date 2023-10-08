6f2e3a31e4fef2e9fe5040ecb8585d7275a528b5
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2023-10-08 19:05:20 -0400

    Server Creation FrontEnd

---
+++ components/file-upload.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  endpoint: "messageFile" | "serverImage";
+  value: string;
+  onChange: (url?: string) => void;
+}
+
+export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image
+          alt="Upload"
+          src={value}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
+        <button
+          className="bg-rose-500 text-white rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+          onClick={() => onChange("")}
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.error(error);
+      }}
+    />
+  );
+};
